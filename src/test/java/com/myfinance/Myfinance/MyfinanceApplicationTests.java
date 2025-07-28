8167e1f8aacf6ffcaf455d67b334a18a1f265f7b
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-07-29 00:32:19 +0530

    new

---
+++ src/test/java/com/myfinance/Myfinance/MyfinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myfinance.Myfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
