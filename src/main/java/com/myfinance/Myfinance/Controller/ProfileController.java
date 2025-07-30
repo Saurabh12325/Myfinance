96d26ff88c3ad44940c4ec5c800e6c32f7becba0
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-07-30 23:34:55 +0530

    changes

--- src/main/java/com/myfinance/Myfinance/Controller/ProfileController.java
+++ src/main/java/com/myfinance/Myfinance/Controller/ProfileController.java
@@ -1,9 +1,14 @@
 package com.myfinance.Myfinance.Controller;
 
 import com.myfinance.Myfinance.Service.ProfileService;
+
 import com.myfinance.Myfinance.dto.ProfileDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +19,10 @@ public class ProfileController {
 
     private final ProfileService profileService;
 
-    public ResponseEntity<ProfileDTO> registerProfile(ProfileDTO profileDTO) {
+@PostMapping("/register")
+    public ResponseEntity<ProfileDTO> registerProfile( @RequestBody ProfileDTO profileDTO) {
         ProfileDTO profile = profileService.registerProfile(profileDTO);
-        return ResponseEntity.ok(profile);
+        return ResponseEntity.status(HttpStatus.CREATED).body(profile);
     }
 
 }
---
+++ src/main/java/com/myfinance/Myfinance/Service/EmailService.java
@@ -0,0 +1,17 @@
+package com.myfinance.Myfinance.Service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+    private final JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    public  void sendEmail(String to, String subject, String body) {
+
+    }
+}
