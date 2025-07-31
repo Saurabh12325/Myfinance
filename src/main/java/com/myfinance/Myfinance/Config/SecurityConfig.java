2117c541ba0a395371143ec2a065a07b4944272c
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-07-31 23:04:04 +0530

    changes

--- src/main/java/com/myfinance/Myfinance/Config/SecurityConfig.java
+++ src/main/java/com/myfinance/Myfinance/Config/SecurityConfig.java
@@ -6,7 +6,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -18,9 +26,25 @@ public class SecurityConfig {
         http.cors(Customizer.withDefaults())
         .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth ->
-                        auth.requestMatchers("/staus","/profile/register","/profile/activate").permitAll()
-                                .requestMatchers("/profile/**").authenticated()
+                        auth.requestMatchers("/status","/profile/register","/profile/activate","/profile/login").permitAll()
+                                .anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
 
     }
-
+   @Bean
+    public PasswordEncoder passwordEncoder() {
+       return new BCryptPasswordEncoder();
+   }
+   @Bean
+    public CorsConfiguration corsConfiguration() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+       corsConfiguration.setAllowedOriginPatterns(List.of("*"));
+       corsConfiguration.setAllowedMethods(List.of("GET","POST","PUT","DELETE"));
+       corsConfiguration.setAllowedHeaders(List.of("Authorization","Content-Type","Accept"));
+       corsConfiguration.setAllowCredentials(true);
+       UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+       source.registerCorsConfiguration("/**",corsConfiguration);
+       return corsConfiguration;
+   }
 }
