ead681770a4d7bd89a641329c13182567d7c70f6
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-08-03 12:12:23 +0530

    changes

--- src/main/java/com/myfinance/Myfinance/Util/JwtUtil.java
+++ src/main/java/com/myfinance/Myfinance/Util/JwtUtil.java
@@ -1,4 +1,28 @@
 package com.myfinance.Myfinance.Util;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
 public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+    private String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
 }
