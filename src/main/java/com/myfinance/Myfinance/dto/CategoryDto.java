ff9e7387fc4404eb3aa5b091129e9bd7cc2cf6ee
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-08-12 00:58:52 +0530

    changes

--- src/main/java/com/myfinance/Myfinance/dto/CategoryDto.java
+++ src/main/java/com/myfinance/Myfinance/dto/CategoryDto.java
@@ -1,4 +1,23 @@
 package com.myfinance.Myfinance.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class CategoryDto {
+    private Long id;
+    private String profileId;
+    private String name;
+    private String type;
+    private String icon;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
