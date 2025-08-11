df90be1cc369eb6c107f8f0a9e11fb890da69e2c
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-08-12 00:54:48 +0530

    changes

---
+++ src/main/java/com/myfinance/Myfinance/Entity/CategoryEntity.java
@@ -0,0 +1,32 @@
+package com.myfinance.Myfinance.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "category")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Column(updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @CreationTimestamp
+    private LocalDateTime updatedAt;
+    private String type;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "profile_id",nullable = false)
+    private ProfileEntity profile;
+}
