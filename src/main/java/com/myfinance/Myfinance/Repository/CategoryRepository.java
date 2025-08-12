95aaef540aa528253f51b115685e66c235b38e90
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-08-12 16:49:46 +0530

    changes

---
+++ src/main/java/com/myfinance/Myfinance/Repository/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.myfinance.Myfinance.Repository;
+
+import com.myfinance.Myfinance.Entity.CategoryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CategoryRepository extends JpaRepository<CategoryEntity , Long> {
+
+    List<CategoryEntity> findByProfileId(Long profileId);
+}
