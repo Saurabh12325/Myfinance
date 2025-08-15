d325823abe8740ed3f001c505a6a1f9b0c613f75
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-08-15 08:10:38 +0530

    making category

--- src/main/java/com/myfinance/Myfinance/Service/CategoryService.java
+++ src/main/java/com/myfinance/Myfinance/Service/CategoryService.java
@@ -1,6 +1,8 @@
 package com.myfinance.Myfinance.Service;
 
+import com.myfinance.Myfinance.Entity.ProfileEntity;
 import com.myfinance.Myfinance.Repository.CategoryRepository;
+import com.myfinance.Myfinance.dto.CategoryDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +11,10 @@ import org.springframework.stereotype.Service;
 public class CategoryService {
     private final ProfileService profileService;
     private final CategoryRepository categoryRepository;
+
+    public CategoryDto saveCategory(CategoryDto categoryDto) {
+        ProfileEntity profile = profileService.getCurrentProfile();
+        if(categoryRepository.existsByNameAndProfileId(categoryDto.getName(),profile.getId())) {
+
+    }
 }
