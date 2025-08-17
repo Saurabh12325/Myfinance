ead1e42160f9564fd5993be6b2f8c053e0a02fca
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-08-17 23:39:06 +0530

    making category

--- src/main/java/com/myfinance/Myfinance/Controller/CategoryController.java
+++ src/main/java/com/myfinance/Myfinance/Controller/CategoryController.java
@@ -5,10 +5,7 @@ import com.myfinance.Myfinance.dto.CategoryDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,4 +19,6 @@ public class CategoryController {
         CategoryDto savedCategory = categoryService.saveCategory(categoryDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedCategory);
     }
+
+    @GetMapping
 }
