2c033334f838aaf75337c38efcfcda76e8ef6f56
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-08-22 17:40:42 +0530

    making expense

--- src/main/java/com/myfinance/Myfinance/dto/ExpenseDto.java
+++ src/main/java/com/myfinance/Myfinance/dto/ExpenseDto.java
@@ -1,4 +1,16 @@
 package com.myfinance.Myfinance.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ExpenseDto {
 }
--- src/main/java/com/myfinance/Myfinance/dto/IncomeDto.java
+++ src/main/java/com/myfinance/Myfinance/dto/IncomeDto.java
@@ -1,4 +1,25 @@
 package com.myfinance.Myfinance.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class IncomeDto {
+    private Long id;
+    private String name;
+    private String icon;
+    private String CategoryName;
+    private String CategoryId;
+    private LocalDate date;
+    private BigDecimal amount;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
