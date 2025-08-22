857944da6bac5ca06fbfe48a409c77e9b3d15595
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-08-22 17:08:58 +0530

    making expense

--- src/main/java/com/myfinance/Myfinance/Entity/ExpenseEntity.java
+++ src/main/java/com/myfinance/Myfinance/Entity/ExpenseEntity.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -17,12 +19,17 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "tbl_income")
 public class ExpenseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
    private Long id;
    private String name;
    private String icon;
    private LocalDate date;
    private BigDecimal amount;
+   @Column(updatable = false)
+   @CreationTimestamp
    private LocalDateTime createdAt;
+   @UpdateTimestamp
    private LocalDateTime updatedAt;
    @ManyToOne(fetch = FetchType.LAZY)
    @JoinColumn(name = "profile_id",nullable = false)
@@ -31,4 +38,11 @@ public class ExpenseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id",nullable = false)
    private CategoryEntity category;
+
+    @PrePersist
+    public void prePersist() {
+        if(this.date == null) {
+            this.date = LocalDate.now();
+        }
+    }
 }
