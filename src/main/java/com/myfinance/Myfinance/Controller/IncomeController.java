3da56ea8f9fcb4386ebbcf8d45de87732d15b4db
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-08-23 14:53:47 +0530

    changes in income

---
+++ src/main/java/com/myfinance/Myfinance/Controller/IncomeController.java
@@ -0,0 +1,4 @@
+package com.myfinance.Myfinance.Controller;
+
+public class IncomeController {
+}
--- src/main/java/com/myfinance/Myfinance/Service/IncomeService.java
+++ src/main/java/com/myfinance/Myfinance/Service/IncomeService.java
@@ -1,13 +1,12 @@
 package com.myfinance.Myfinance.Service;
 
 import com.myfinance.Myfinance.Entity.CategoryEntity;
-import com.myfinance.Myfinance.Entity.ExpenseEntity;
 import com.myfinance.Myfinance.Entity.IncomeEntity;
 import com.myfinance.Myfinance.Entity.ProfileEntity;
 import com.myfinance.Myfinance.Mapper.Mapper;
 import com.myfinance.Myfinance.Repository.CategoryRepository;
 import com.myfinance.Myfinance.Repository.IncomeRepository;
-import com.myfinance.Myfinance.dto.ExpenseDto;
+
 import com.myfinance.Myfinance.dto.IncomeDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,6 +17,7 @@ public class IncomeService {
     private final ProfileService profileService;
     private final CategoryRepository categoryRepository;
     private final IncomeRepository incomeRepository;
+
     public IncomeDto addExpense(IncomeDto dto){
         ProfileEntity profile = profileService.getCurrentProfile();
         CategoryEntity category = categoryRepository.findById(dto.getCategoryId()).orElseThrow(() -> new RuntimeException("Category not found"));
