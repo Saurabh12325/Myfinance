1c03d4493b67038064dedebb99d888c0aa47373d
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-08-23 14:52:02 +0530

    changes in income

--- src/main/java/com/myfinance/Myfinance/Service/ExpenseService.java
+++ src/main/java/com/myfinance/Myfinance/Service/ExpenseService.java
@@ -1,9 +1,30 @@
 package com.myfinance.Myfinance.Service;
 
+import com.myfinance.Myfinance.Entity.CategoryEntity;
+import com.myfinance.Myfinance.Entity.ExpenseEntity;
+import com.myfinance.Myfinance.Entity.ProfileEntity;
+import com.myfinance.Myfinance.Mapper.Mapper;
+import com.myfinance.Myfinance.Repository.CategoryRepository;
+import com.myfinance.Myfinance.Repository.ExpenseRepository;
+import com.myfinance.Myfinance.dto.ExpenseDto;
+import com.myfinance.Myfinance.dto.IncomeDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ExpenseService {
 
+    private final ProfileService profileService;
+    private final ExpenseRepository expenseRepository;
+    private final CategoryRepository categoryRepository;
+
+    public ExpenseDto addExpense(ExpenseDto dto){
+        ProfileEntity profile = profileService.getCurrentProfile();
+        CategoryEntity category = categoryRepository.findById(dto.getCategoryId()).orElseThrow(() -> new RuntimeException("Category not found"));
+        ExpenseEntity newExpense = Mapper.mapToExpenseEntity(dto,profile,category);
+        newExpense = expenseRepository.save(newExpense) ;
+        return Mapper.mapToExpenseDto(newExpense);
+    }
 
 }
--- src/main/java/com/myfinance/Myfinance/Service/IncomeService.java
+++ src/main/java/com/myfinance/Myfinance/Service/IncomeService.java
@@ -1,4 +1,28 @@
 package com.myfinance.Myfinance.Service;
 
+import com.myfinance.Myfinance.Entity.CategoryEntity;
+import com.myfinance.Myfinance.Entity.ExpenseEntity;
+import com.myfinance.Myfinance.Entity.IncomeEntity;
+import com.myfinance.Myfinance.Entity.ProfileEntity;
+import com.myfinance.Myfinance.Mapper.Mapper;
+import com.myfinance.Myfinance.Repository.CategoryRepository;
+import com.myfinance.Myfinance.Repository.IncomeRepository;
+import com.myfinance.Myfinance.dto.ExpenseDto;
+import com.myfinance.Myfinance.dto.IncomeDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class IncomeService {
+    private final ProfileService profileService;
+    private final CategoryRepository categoryRepository;
+    private final IncomeRepository incomeRepository;
+    public IncomeDto addExpense(IncomeDto dto){
+        ProfileEntity profile = profileService.getCurrentProfile();
+        CategoryEntity category = categoryRepository.findById(dto.getCategoryId()).orElseThrow(() -> new RuntimeException("Category not found"));
+        IncomeEntity newExpense = Mapper.mapToIncomeEntity(dto,profile,category);
+        newExpense = incomeRepository.save(newExpense) ;
+        return Mapper.mapToIncomeDto(newExpense);
+    }
 }
