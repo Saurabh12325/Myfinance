ad064f62ba34df4f458406776293bc3ccd26a25f
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-08-23 22:46:03 +0530

    changes in income

--- src/main/java/com/myfinance/Myfinance/dto/RecentTransactionDto.java
+++ src/main/java/com/myfinance/Myfinance/dto/RecentTransactionDto.java
@@ -1,11 +1,18 @@
 package com.myfinance.Myfinance.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.cglib.core.Local;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class RecentTransactionDto {
     private Long id;
     private String name;
