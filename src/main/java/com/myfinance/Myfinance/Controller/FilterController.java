e838eeb7b652278eb2c04b4d46528c450f94d148
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-08-26 12:00:20 +0530

    filter

---
+++ src/main/java/com/myfinance/Myfinance/Controller/FilterController.java
@@ -0,0 +1,12 @@
+package com.myfinance.Myfinance.Controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/filter")
+@RequiredArgsConstructor
+public class FilterController {
+
+}
