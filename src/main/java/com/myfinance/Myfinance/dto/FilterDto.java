8d89983ef207f0b7e310cf9e3790c1f3c45f720e
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-08-26 11:56:52 +0530

    filter

--- src/main/java/com/myfinance/Myfinance/dto/FilterDto.java
+++ src/main/java/com/myfinance/Myfinance/dto/FilterDto.java
@@ -1,4 +1,15 @@
 package com.myfinance.Myfinance.dto;
 
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
 public class FilterDto {
+    private String type;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String keyword;
+    private String sortField;  // date , amount , name
+    private String sortOrder;  // des or asc
 }
